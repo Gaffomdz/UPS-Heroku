c3c6f7dcf107e04c0c58f422a2ebd9ffdea0ade6
Giorgio <giorgiogirardelli@gmail.com>
2022-10-02 14:51:16 -0300

    2

--- src/utils/wemeta.ts
+++
@@ -1,7 +0,0 @@
-import { movePlayerTo } from "@decentraland/RestrictedActions";
-import { WeMeta } from "@wemeta/analytics"
-  
-// new WeMeta('b9780cbb-1379-49fd-9d3f-955b7b40b222');
-new WeMeta('ab3ae2d3-8c58-46a2-a324-6dc19368baf0');
-
-movePlayerTo
